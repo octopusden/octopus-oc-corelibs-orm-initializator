4abdf95120eafd12834791a22a1bb9fc050c3373
Ivan Anisimov <ivansa@me.com>
2023-04-14 12:35:26 +0300

    add build wf

--- setup.py
+++ setup.py
@@ -2,15 +2,21 @@
 
 from setuptools import setup
 
-__version="0.0.6.1"
+__version="0.0.1"
 
 _spec = {
     "name" : "oc_orm_initializator",
     "packages" : ["oc_orm_initializator"],
     "version" : __version,
-    "install_requires": ["django==3.2.13", "django-simple-history==3.0.0", "psycopg2"],
+    "install_requires": [
+        "django==3.2.13", 
+        "django-simple-history==3.0.0", 
+        "psycopg2"
+    ],
     "python_requires": ">=3.6",
     "description" : "Django ORM intialization",
+    "long_description": "",
+    "long_description_content_type": "text/plain"
 }
 
 setup(**_spec)
